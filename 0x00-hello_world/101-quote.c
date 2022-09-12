f7a3979b961b6a2fa9c4fe786c4522469a8d428d
sharon <sharonrinzee@gmail.com>
2022-09-12 16:36:44 -0700

    Fix

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+/**
+ *main - print size of data
+ *
+ * Return: Always 0
+ * */
+int main(void)
+{
+	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+	return (1);
+}
