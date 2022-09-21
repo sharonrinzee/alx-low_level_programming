b8d1fc7ed00937d9abbafc68d3aa6dbcfeb52ae5
sharon <sharonrinzee@gmail.com>
2022-09-21 15:30:55 -0700

    Fix

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * Main - Entry point
+ *
+ * Return: Always 0 (Success)
+ *
+ */
+int main(void)
+{
+	int a;
+	char s;
+
+	for (a = '0'; a <= '9'; a++)
+		putchar(a);
+	for (s = 'a'; s <= 'f'; s++)
+		putchar(s);
+	putchar('\n');
+	return (0);
+}
