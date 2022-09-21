57d0fa85cca919a6d63fb4c237bc7b8e1203205e
sharon <sharonrinzee@gmail.com>
2022-09-20 18:00:36 -0700

    Fix

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Entry Point
+ *
+ * Return: Always 0 (Success)
+ *
+ */
+int main(void)
+{
+	int m;
+
+	for (m = '0'; m <= '9'; m++)
+		putchar(m);
+	putchar('\n');
+
+	return (0);
+}
